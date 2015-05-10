b926ff882e47cd85dcb6272bb72af165f649e8e4
Brian London <brian@Brians-iMac.local>
2015-05-10 13:58:10 -0600

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Brian London on 5/10/15.
+//  Copyright (c) 2015 Brian London. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
